818b4f71186dd70f214dbdf1420587615c887703
bl00dclot <discryptment@tutamail.com>
2025-09-07 18:57:45 +0400

    vault_state and vault_history in DB

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,24 @@
-CREATE TABLE IF NOT EXISTS vault (
-    id INTEGER,
-    usd DOUBLE,
-    gold INTEGER
+CREATE TABLE IF NOT EXISTS vault_state (
+    price_per_g INTEGER,
+    gold_g BIGINT,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_by BIGINT,
+    version BIGINT DEFAULT 1
+);
+CREATE TABLE IF NOT EXISTS vault_history (
+    id BIGINT,
+    action VARCHAR,
+    change_gold_g BIGINT,
+    prev_gold_g BIGINT,
+    new_gold_g BIGINT,
+    prev_price_per_g BIGINT,
+    new_price_per_g BIGINT,
+    prev_value_usd BIGINT, --prev_price_per_g * prev_gold_g
+    new_value_usd BIGINT, --new_gold_g * new_price_per_g
+    actor_telegram_id BIGINT,
+    reason VARCHAR,
+    external_id VARCHAR,
+    created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE TABLE IF NOT EXISTS config (
     id INTEGER,
@@ -20,9 +37,9 @@ CREATE TABLE IF NOT EXISTS users (
     telegram_id BIGINT,
     username VARCHAR,
     gold_balance INTEGER,
-    real_usd_balance DOUBLE,
+    real_usd_balance INTEGER,
     gold_price_usd INT,
-    expected_usd_total DOUBLE,
+    expected_usd_total INTEGER,
     authorized BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -32,7 +49,7 @@ CREATE TABLE IF NOT EXISTS invoices (
     user_id INTEGER,
     contract_name VARCHAR,
     gold INTEGER,
-    expected_usd DOUBLE,
-    received_usd DOUBLE,
+    expected_usd INTEGER,
+    received_usd INTEGER,
     timestamp TIMESTAMP
 );
